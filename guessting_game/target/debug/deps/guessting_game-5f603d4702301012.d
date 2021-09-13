a86133fad56e977a430517a28991dad319fd9fce
8-u8 <itotaka523@gmail.com>
2021-09-13 19:09:17 +0900

    changed gitignore.

--- guessting_game/target/debug/deps/guessting_game-5f603d4702301012.d
+++
@@ -1,5 +0,0 @@
-/home/t0_u0/Desktop/learning_Rust/guessting_game/target/debug/deps/guessting_game-5f603d4702301012: src/main.rs
-
-/home/t0_u0/Desktop/learning_Rust/guessting_game/target/debug/deps/guessting_game-5f603d4702301012.d: src/main.rs
-
-src/main.rs:
--- guessting_game/target/debug/guessting_game.d
+++
@@ -1,1 +0,0 @@
-/home/t0_u0/Desktop/learning_Rust/guessting_game/target/debug/guessting_game: /home/t0_u0/Desktop/learning_Rust/guessting_game/src/main.rs
